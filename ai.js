34c8b79893d7586136a35870736199b9f25f2fed
Yoann Cribier <ycribier@student.42.fr>
2017-07-26 11:04:47 +0200

    snail

--- ai.js
+++ ai.js
@@ -7,6 +7,12 @@ const directions = [
   { x: 0, y: 1 },  // down
   { x: -1, y: 0 }, // left
 ]
+const d = directions
+
+const UP    = 0
+const RIGHT = 1
+const DOWN  = 2
+const LEFT  = 3
 
 const pickRandom = arr => arr[Math.floor(Math.random() * arr.length)]
 const getRandomFreeSlot = (getPos, isFree) => pickRandom(directions
@@ -14,9 +20,58 @@ const getRandomFreeSlot = (getPos, isFree) => pickRandom(directions
   .filter(isFree))
 
 
+let radius = 1
+let currentDirection = UP
+
 // you should return a function that is called with those 4 tools functions
 // and your player object
 return ({ isFree, getDist, reduceMap, getPos }) => {
 
-  return 'down' //getRandomFreeSlot(getPos, isFree) || 'up'
+  // isFree({x: 0, y: 0})
+  // reduceMap((x, y, acc) => {})
+  // getDist({x,y}, {x,y})
+  // getPos() -> {x, y}
+
+  switch (currentDirection) {
+
+  case UP:
+    if (isFree(getPos(D[RIGHT]))) {
+      currentDirection = RIGHT
+      return 'right'
+    }
+    return 'up'
+
+
+  case RIGHT:
+    if (isFree(getPos(D[DOWN]))) {
+      currentDirection = DOWN
+      return 'down'
+    }
+    return 'right'
+
+
+  case DOWN:
+    if (isFree(getPos(D[LEFT]))) {
+      currentDirection = LEFT
+      return 'left'
+    }
+    return 'down'
+
+
+  case LEFT:
+    if (isFree(getPos(D[UP]))) {
+      currentDirection = UP
+      return 'up'
+    }
+    return 'left'
+
+  }
+
+  return getRandomFreeSlot(getPos, isFree) || 'up'
 }
+
+xoxxx
+xooox
+xoxox
+xooox
+xxxxx
