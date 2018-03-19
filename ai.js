28a333c423904ed794f5f92a728ba12c95840be2
Clement Denis <le.mikmac@gmail.com>
2018-03-19 20:03:22 +0100

    update ai to webworker

--- ai.js
+++ ai.js
@@ -1,30 +1,46 @@
-SIZE  //available size of the map
-
-// here you can setup any functions you will use in your final solve function
-const directions = [
-  { x: 0, y: -1 }, // up
-  { x: 1, y: 0 },  // right
-  { x: 0, y: 1 },  // down
-  { x: -1, y: 0 }, // left
-]
-
-const pickRandom = arr => arr[Math.floor(Math.random() * arr.length)]
-const getRandomFreeSlot = (getPos, isFree) => pickRandom(directions
-  .map(getPos)
-  .filter(isFree))
-
-
-// you should return a function that is called with those 4 tools functions
-// and the players array
-// a player is an object such as
-// {
-//   name,
-//   score,
-//   dead, // boolean
-//   x,
-//   y,
-// }
-return ({ isFree, getDist, reduceMap, getPos, players }) => {
-
-  return getRandomFreeSlot(getPos, isFree) || 'up'
+const SIZE = 100
+const DIR = {
+  up:    { x:  0, y: -1 },
+  down:  { x:  0, y:  1 },
+  left:  { x: -1, y:  0 },
+  right: { x:  1, y:  0 },
 }
+
+// Bunch of utils function to get you going
+const pickRandom = arr => arr[Math.floor(Math.random() * arr.length)]
+const notInBounds = n => n >= SIZE || n < 0
+const inBounds = n => n < SIZE && n >= 0
+const dist = (a, b) => Math.abs(a - b)
+const getDist = (a, b) => dist(a.x, b.x) + dist(a.y, b.y)
+const isPlayerDead = p => p.dead
+const fold = (fn, acc) => {
+  // fold call the given function for every block
+  // with x, y and an optionnal accumulator (much like reduce)
+  let x = -1, y
+  while (++x < SIZE) {
+    y = -1
+    while (++y < SIZE) {
+      acc = fn(x, y, acc)
+    }
+  }
+  return acc
+}
+
+const directionValues = Object.values(DIR)
+
+const getAvailableDirections = (getPos, isFree) => directionValues
+  .map(getPos)
+  .filter(isFree)
+
+// TODO: handle the seed
+addEventListener('message', event => {
+  const { map, players, id } = JSON.parse(event.data)
+  console.log({ map, players, id })
+  // Ensure we are sending the answer only once
+  const moveTo = dir => event._done || (event._done = true, postMessage(dir))
+
+  moveTo(pickRandom(getAvailableDirections(getPos, isFree)) || 'up')
+})
+
+// we tell the main thread that the AI is loaded successfully
+sendMessage('loaded')
