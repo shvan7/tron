2fea9b603c29bfb2c7079bf028d4492f898a5a63
Clement Denis <le.mikmac@gmail.com>
2018-03-23 15:21:05 +0100

    handle ai load fallback

--- ai-loader.js
+++ ai-loader.js
@@ -1,17 +1,22 @@
 const getShaUrl = login =>
   `https://api.github.com/repos/${login}/tron/commits/master`
 
-const getAIUrl = (login, sha) =>
-  `https://rawgit.com/${login}/tron/${sha}/ai/${login}.js`
+const getAIUrl = (login, sha, ai) =>
+  `https://rawgit.com/${login}/tron/${sha}/ai/${ai || login}.js`
 
-const handleLogin404 = async res => (res.ok
-  ? (await fetch(getShaUrl('nan-academy')))
-  : res).json().sha
+const getUrl = async (login, sha) => {
+  if (location.hostname.endsWith('.github.io')) {
+    return `${location.origin}/ai/${login}.js`
+  }
 
-const getUrl = async (login, sha) => !location.hostname.endsWith('.github.io')
-  ? `${location.origin}/ai/${login}.js`
-  : getAIUrl(login, sha || (sha = (await fetch(getShaUrl(login))
-      .then(handleLogin404))))
+  if (sha) return getAIUrl(login, sha)
+
+  const res = await fetch(getShaUrl(login))
+  if (res.ok) return getAIUrl(login, (await res.json()).sha)
+
+  const sha = (await (await fetch(getShaUrl('nan-academy'))).json()).sha
+  return getAIUrl('nan-academy', sha, login)
+}
 
 export default async ({ name, id, sha, seed }) => {
   const w = new Worker(await getUrl(name, sha), { type: 'module' })
