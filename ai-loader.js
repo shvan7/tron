05bce51370c4be1845412b80ede6fe9124701310
Clement Denis <le.mikmac@gmail.com>
2018-03-23 15:12:00 +0100

    load ai fallback

--- ai-loader.js
+++ ai-loader.js
@@ -4,10 +4,14 @@ const getShaUrl = login =>
 const getAIUrl = (login, sha) =>
   `https://rawgit.com/${login}/tron/${sha}/ai/${login}.js`
 
+const handleLogin404 = async res => (res.ok
+  ? (await fetch(getShaUrl('nan-academy')))
+  : res).json().sha
+
 const getUrl = async (login, sha) => !location.hostname.endsWith('.github.io')
   ? `${location.origin}/ai/${login}.js`
-  : getAIUrl(login, sha
-                || (sha = (await (await fetch(getShaUrl(login))).json()).sha))
+  : getAIUrl(login, sha || (sha = (await fetch(getShaUrl(login))
+      .then(handleLogin404))))
 
 export default async ({ name, id, sha, seed }) => {
   const w = new Worker(await getUrl(name, sha), { type: 'module' })
